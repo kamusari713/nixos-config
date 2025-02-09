7ee97150f349ca928e889421e8827374725b0e80
kamusari1337 <ovsyannikovkostyan@gmail.com>
2025-02-09 18:49:19 +0400

    refactor

--- flake.nix
+++ flake.nix
@@ -1,51 +1,52 @@
 {
-    description = "My nixos flake";
 
-    inputs = {
+  description = "My nixos flake";
 
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
 
-        home-manager = {
-            url = "github:nix-community/home-manager/release-24.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = {
-        nixpkgs,
-        nixpkgs-unstable,
-        home-manager,
-        ... } @inputs:
-    let
-        lib = nixpkgs.lib;
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in {
+  };
 
-        homeConfigurations.kamusari = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = { inherit inputs; };
-            modules = [
-                ./home-manager/home.nix
-            ];
-        };
+  outputs = {
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    ... } @inputs:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
 
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            specialArgs = {
-                inherit inputs;
-                unstable = import inputs.nixpkgs-unstable {
-                    inherit system;
-                    config.allowUnfree = true;
-                };
-            };
-
-            modules = [
-                ./modules/configuration.nix
-            ];
-        };
-
+    homeConfigurations.kamusari = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = { inherit inputs; };
+      modules = [
+        ./home-manager/home.nix
+      ];
     };
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs;
+        unstable = import inputs.nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+
+      modules = [
+        ./modules/configuration.nix
+      ];
+    };
+
+  };
 
 }
--- modules/configuration.nix
+++ modules/configuration.nix
@@ -2,217 +2,217 @@
 
 {
 
-    imports =
-    [
-        ./hardware-configuration.nix
-        inputs.home-manager.nixosModules.default
+  imports =
+  [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
+
+  home-manager = {
+    users = {
+      kamusari = import ../home-manager/home.nix;
+    };
+  };
+
+  disabledModules = [ "services/networking/zapret.nix" ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  networking.nftables.enable = true;
+
+  hardware = {
+    graphics.enable = true;
+    nvidia ={
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = false;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+  };
+
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      efi.efiSysMountPoint = "/boot";
+    };
+    kernelPackages = pkgs.linuxPackages;
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+    };
+  };
+
+  services = {
+    openssh.enable = true;
+
+    redis = {
+      servers = {
+        kaidan-redis-dev = {
+          enable = true;
+          port = 6379;
+        };
+      };
+    };
+
+    mongodb = {
+      enable = true;
+      package = pkgs.mongodb-ce;
+      bind_ip = "127.0.0.1";
+    };
+
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      xkb.layout = "us,ru";
+      videoDrivers = [ "nvidia" ];
+    };
+
+    displayManager.autoLogin = {
+      enable = true;
+      user = "kamusari";
+    };
+
+    libinput.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+  };
+
+  programs = {
+    steam = {
+      enable = true;
+      extraPackages = with pkgs; [ libpulseaudio ];
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+
+    zsh.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = [ "gtk" ];
+      hyprland.default = [
+        "gtk"
+        "hyprland"
+        "wlr"
+      ];
+    };
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
     ];
+  };
 
-    home-manager = {
-        users = {
-            kamusari = import ../home-manager/home.nix;
-        };
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    fira-code
+    iosevka
+    montserrat
+    (nerdfonts.override { fonts = [ "Iosevka" ]; })
+  ];
+
+  time.timeZone = "Europe/Astrakhan";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ru_RU.UTF-8";
+      LC_IDENTIFICATION = "ru_RU.UTF-8";
+      LC_MEASUREMENT = "ru_RU.UTF-8";
+      LC_MONETARY = "ru_RU.UTF-8";
+      LC_NAME = "ru_RU.UTF-8";
+      LC_NUMERIC = "ru_RU.UTF-8";
+      LC_PAPER = "ru_RU.UTF-8";
+      LC_TELEPHONE = "ru_RU.UTF-8";
+      LC_TIME = "ru_RU.UTF-8";
     };
+  };
 
-    disabledModules = [ "services/networking/zapret.nix" ];
+  users.users.kamusari = {
+    isNormalUser = true;
+    extraGroups = [ "docker" "audio" "input" "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+  };
 
-    nix.gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 7d";
-    };
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    kitty
+    pavucontrol
+    home-manager
 
-    networking.nftables.enable = true;
+    nautilus
+    waybar
+    rofi-wayland
+    bibata-cursors
 
-    hardware = {
-        graphics.enable = true;
-        nvidia ={
-            modesetting.enable = true;
-            powerManagement.enable = false;
-            powerManagement.finegrained = false;
-            open = false;
-            nvidiaSettings = false;
-            package = config.boot.kernelPackages.nvidiaPackages.beta;
-        };
-    };
+    hyprshot
+    swaybg
+    mako
+    wl-clipboard
+    cliphist
 
-    environment.sessionVariables = {
-        WRL_NO_HARDWARE_CURSORS = "1";
-        NIXOS_OZONE_WL = "1";
-    };
+    google-chrome
+    telegram-desktop
+    vesktop
+    protonup
 
-    nix = {
-        settings = {
-            experimental-features = [
-                "nix-command"
-                "flakes"
-            ];
-        };
-    };
+    nodejs_23
+    jdk21
+    maven
+    mongosh
+    mongodb-compass
 
-    nixpkgs.config.allowUnfree = true;
+    cbonsai
+    cmatrix
+    fd
+    htop
+    zsh
+  ] ++ (with unstable; [
+    amnezia-vpn
+    vscode
+  ]);
 
-    boot = {
-        loader = {
-            systemd-boot.enable = true;
-            efi.canTouchEfiVariables = true;
-            efi.efiSysMountPoint = "/boot";
-        };
-        kernelPackages = pkgs.linuxPackages;
-    };
-
-    networking = {
-        hostName = "nixos";
-        networkmanager.enable = true;
-        firewall.enable = false;
-    };
-
-    services = {
-        openssh.enable = true;
-
-        xserver = {
-            enable = true;
-            displayManager.gdm.enable = true;
-            xkb.layout = "us,ru";
-            videoDrivers = [ "nvidia" ];
-        };
-
-        displayManager.autoLogin = {
-            enable = true;
-            user = "kamusari";
-        };
-
-        libinput.enable = true;
-
-        pipewire = {
-            enable = true;
-            alsa.enable = true;
-            alsa.support32Bit = true;
-            pulse.enable = true;
-            wireplumber.enable = true;
-        };
-    };
-
-    environment.etc."wireplumber/main.lua.d/50-disable-auto-adjust.lua".text = ''
-    table.insert(config.rules, {
-        matches = {
-            {
-                -- Отключает автонастройку микрофона
-                { "node.name", "matches", "alsa_input.*" }
-            }
-        },
-        apply_properties = {
-            ["session.suspend-timeout-seconds"] = 0,
-            ["node.nick"] = "Disabled Auto Adjust",
-            ["audio.adaptive-resampler"] = false
-        }
-    })
-    '';
-
-    programs = {
-        steam = {
-            enable = true;
-            extraPackages = with pkgs; [ libpulseaudio ];
-            remotePlay.openFirewall = true;
-            dedicatedServer.openFirewall = true;
-            localNetworkGameTransfers.openFirewall = true;
-        };
-
-        hyprland = {
-            enable = true;
-            xwayland.enable = true;
-        };
-
-        zsh.enable = true;
-    };
-
-    xdg.portal = {
-        enable = true;
-        xdgOpenUsePortal = true;
-        config = {
-            common.default = [ "gtk" ];
-            hyprland.default = [
-                "gtk"
-                "hyprland"
-                "wlr"
-            ];
-        };
-        extraPortals = [
-            pkgs.xdg-desktop-portal-gtk
-            pkgs.xdg-desktop-portal-hyprland
-        ];
-    };
-
-    fonts.packages = with pkgs; [
-        jetbrains-mono
-        fira-code
-        iosevka
-        montserrat
-        (nerdfonts.override { fonts = [ "Iosevka" ]; })
-    ];
-
-    time.timeZone = "Europe/Astrakhan";
-
-    i18n = {
-        defaultLocale = "en_US.UTF-8";
-        extraLocaleSettings = {
-            LC_ADDRESS = "ru_RU.UTF-8";
-            LC_IDENTIFICATION = "ru_RU.UTF-8";
-            LC_MEASUREMENT = "ru_RU.UTF-8";
-            LC_MONETARY = "ru_RU.UTF-8";
-            LC_NAME = "ru_RU.UTF-8";
-            LC_NUMERIC = "ru_RU.UTF-8";
-            LC_PAPER = "ru_RU.UTF-8";
-            LC_TELEPHONE = "ru_RU.UTF-8";
-            LC_TIME = "ru_RU.UTF-8";
-        };
-    };
-
-    users.users.kamusari = {
-        isNormalUser = true;
-        extraGroups = [ "audio" "input" "networkmanager" "wheel" ];
-        shell = pkgs.zsh;
-    };
-
-    environment.systemPackages = with pkgs; [
-        wget
-        git
-        kitty
-        pavucontrol
-        home-manager
-
-        nautilus
-        waybar
-        rofi-wayland
-        hyprpicker
-        bibata-cursors
-
-        hyprshot
-        swaybg
-        mako
-        wl-clipboard
-        cliphist
-
-        google-chrome
-        telegram-desktop
-        vesktop
-        protonup
-
-        vscode
-        jetbrains.idea-community
-        nodejs_23
-        (jdk21.override { enableJavaFX = true; })
-        maven
-
-        cbonsai
-        cmatrix
-        fd
-        htop
-        zsh
-    ] ++ (with unstable; [
-        amnezia-vpn
-    ]);
-
-    system.stateVersion = "24.11";
+  system.stateVersion = "24.11";
 
 }
